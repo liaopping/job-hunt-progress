69652cc032446df0f12d114135d0c4efc1059089
liaopping <race.lifes.a.race@gmail.com>
2020-11-17 07:39:57 +0900

    refactor(selections): selectionsテーブルのダミーデータ修正
    job_hunterの数を絞り、ひとりひとりが複数のselectionを持つように修正

--- db/fixtures/development/selections.rb
+++ db/fixtures/development/selections.rb
@@ -1,7 +1,7 @@
 puts 'Start inserting seed "selections" ...'
-50.times do |n|
-  job_hunter_id = n+1
-  company_id = n+1
-  job_hunt_service_id = n+1
+50.times do
+  job_hunter_id = rand(1..10)
+  company_id = rand(1..50)
+  job_hunt_service_id = rand(1..5)
   Selection.create!(job_hunter_id: job_hunter_id, company_id: company_id, job_hunt_service_id: job_hunt_service_id)
 end
